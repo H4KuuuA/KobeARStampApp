444ac9b15d777d1def67baa1cfb3fffaffb525b5
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-06-30 12:11:23 +0900

    Cameraボタンにグラデーションを追加

--- KobeARStampApp/Views/MainTabView.swift
+++ KobeARStampApp/Views/MainTabView.swift
@@ -43,16 +43,39 @@ struct MainTabView: View {
                     Button(action:{
                         // カメラ起動
                     }) {
-                        Image(systemName: "camera.circle.fill")
-                            .resizable()
-                            .frame(width: 64, height: 64)
-                            .foregroundColor(.accentColor)
-                            .shadow(radius: 4)
-                    }
+                        ZStack {
+                                Circle()
+                                    .fill(
+                                        LinearGradient(
+                                            gradient: Gradient(colors: [
+                                                Color("LightBlue"),
+                                                Color("DarkBlue")
+                                            ]),
+                                            startPoint: .top,
+                                            endPoint: .bottom
+                                        )
+                                    )
+                                    .frame(width: 72, height: 72) // サイズ調整（円）
+
+                                VStack(spacing: 0) {
+                                    Text("AR")
+                                        .fontWeight(.bold)
+                                        .foregroundColor(.white)
+
+                                    Image(systemName: "camera")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .fontWeight(.medium)
+                                        .frame(width: 42, height: 42)
+                                        .foregroundColor(.white)
+                                }
+                            }
+                        }
+                        .shadow(color: Color.black.opacity(0.2), radius: 4, x: 0, y: 2)
                     
                     Spacer()
                 }
-                .padding(.bottom,8)
+                .padding(.bottom,6)
             }
         }
     }
