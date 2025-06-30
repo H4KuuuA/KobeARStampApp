e404557575b20874e478c9d34eabf22ca019d7b3
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-06-30 11:16:04 +0900

    MainTabViewファイルを追加

---
+++ KobeARStampApp/Views/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  TabView.swift
+//  KobeARStampApp
+//
+//  Created by 大江悠都 on 2025/06/30.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
