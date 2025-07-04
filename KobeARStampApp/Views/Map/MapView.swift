dc57720e14d5e76edc0741927e5b3c54c8add52d
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-07-04 14:48:48 +0900

    mapViewを表示するためにコードを修正

--- KobeARStampApp/Views/Map/MapView.swift
+++ KobeARStampApp/Views/Map/MapView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    @State private var cameraPosition : MapCameraPosition = .region (
+        MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.7050, longitude: 135.2410),
+        span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        )
+    )
+    @ObservedObject private var locationManager = LocationManager.shared
+    
     var body: some View {
-        Map()
+        Map(position: $cameraPosition) {
+            UserAnnotation()
+            // 必要なら他のピンもここで追加可能
+        }
+        .mapControls {
+            MapUserLocationButton()
+            MapCompass()
+        }
+        .onReceive(locationManager.$latitude.combineLatest(locationManager.$longitude)) { lat, lon in
+                    guard lat != 0.0 && lon != 0.0 else { return }
+
+                    DispatchQueue.main.async {
+                        let region = MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(latitude: lat, longitude: lon),
+                            span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                        )
+                        cameraPosition = .region(region)
+                    }
+                }
+        .onAppear {
+            Task {
+                await locationManager.requestLocationPermissionIfNeeded()
+            }
+        }
+        .ignoresSafeArea()
     }
 }
 
