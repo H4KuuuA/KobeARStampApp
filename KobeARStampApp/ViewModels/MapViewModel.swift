3f2f26ff5987da35eb5c280645817d01e0c9b609
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-07-07 10:08:00 +0900

    中心座標と表示半径を指定するMapViewModelを実装

---
+++ KobeARStampApp/ViewModels/MapViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  MapViewModel.swift
+//  KobeARStampApp
+//
+//  Created by 大江悠都 on 2025/07/07.
+//
+
+import SwiftUI
+import  MapKit
+import Combine
+
+class MapViewModel: ObservableObject {
+    
+    @Published var centerCoordinate = CLLocationCoordinate2D(
+        latitude: 34.70602173020105,
+        longitude: 135.2162279954511
+    )
+    @Published var radiusInMeters: CLLocationDistance = 2000
+    
+    private var cancellables = Set<AnyCancellable>()
+    private let locationManager = LocationManager.shared
+    
+    init() {
+        locationManager.$latitude
+            .combineLatest(locationManager.$longitude)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] (lat: Double, lon: Double) in
+                guard lat != 0.0, lon != 0.0 else { return }
+                // 必要があれば現在地でカメラ位置変更などをここで対応
+            }
+            .store(in: &cancellables)
+    }
+    
+    func requestPermission() async {
+        await locationManager.requestLocationPermissionIfNeeded()
+    }
+}
