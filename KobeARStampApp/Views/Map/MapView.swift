9849ecf4177ed12201b3374b5328d9d0ce0be13e
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-07-07 10:53:36 +0900

    MapViewにRestrictedMapViewを表示

--- KobeARStampApp/Views/Map/MapView.swift
+++ KobeARStampApp/Views/Map/MapView.swift
@@ -5,44 +5,22 @@
 //  Created by 大江悠都 on 2025/07/04.
 //
 
+
 import SwiftUI
 import MapKit
 
 struct MapView: View {
-    @State private var cameraPosition : MapCameraPosition = .region (
-        MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 34.7050, longitude: 135.2410),
-        span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
-        )
-    )
-    @ObservedObject private var locationManager = LocationManager.shared
-    
-    var body: some View {
-        Map(position: $cameraPosition) {
-            UserAnnotation()
-            // 必要なら他のピンもここで追加可能
-        }
-        .mapControls {
-            MapUserLocationButton()
-            MapCompass()
-        }
-        .onReceive(locationManager.$latitude.combineLatest(locationManager.$longitude)) { lat, lon in
-                    guard lat != 0.0 && lon != 0.0 else { return }
+    @StateObject private var viewModel = MapViewModel()
 
-                    DispatchQueue.main.async {
-                        let region = MKCoordinateRegion(
-                            center: CLLocationCoordinate2D(latitude: lat, longitude: lon),
-                            span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
-                        )
-                        cameraPosition = .region(region)
-                    }
-                }
-        .onAppear {
-            Task {
-                await locationManager.requestLocationPermissionIfNeeded()
-            }
+    var body: some View {
+        RestrictedMapView(
+            centerCoordinate: viewModel.centerCoordinate,
+            radiusInMeters: viewModel.radiusInMeters
+        )
+        .edgesIgnoringSafeArea(.all)
+        .task {
+            await viewModel.requestPermission()
         }
-        .ignoresSafeArea()
     }
 }
 
