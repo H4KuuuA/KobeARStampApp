905d94b48a9ec6d55e8c6d58bcfe96a6c33a11e8
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-07-14 10:32:23 +0900

    CustomAnntationViewファイルとAnnotation.PDFを追加

---
+++ KobeARStampApp/Views/Map/CustomAnnotaitonView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomAnnotaitonView.swift
+//  KobeARStampApp
+//
+//  Created by 大江悠都 on 2025/07/14.
+//
+
+import SwiftUI
+
+struct CustomAnnotaitonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomAnnotaitonView()
+}
