92d659297d1bedd9832580352b62c15996a24f17
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-09-17 10:10:58 +0900

    locationrequestの不具合を修正

--- KobeARStampApp/ViewModels/MapViewModel.swift
+++ KobeARStampApp/ViewModels/MapViewModel.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-import  MapKit
+import MapKit
 import Combine
 
 class MapViewModel: ObservableObject {
@@ -19,22 +19,18 @@ class MapViewModel: ObservableObject {
     
     private var cancellables = Set<AnyCancellable>()
     private let locationManager = LocationManager.shared
-    private var didSetCenter = false 
+    private var didSetCenter = false
         
-        init() {
-            locationManager.$latitude
-                .combineLatest(locationManager.$longitude)
-                .receive(on: DispatchQueue.main)
-                .sink { [weak self] (lat: Double, lon: Double) in
-                    guard lat != 0.0, lon != 0.0 else { return }
-                    guard let self = self, !self.didSetCenter else { return }
-                    self.centerCoordinate = CLLocationCoordinate2D(latitude: lat, longitude: lon)
-                    self.didSetCenter = true
-                }
-                .store(in: &cancellables)
-        }
-    
-    func requestPermission() async {
-        await locationManager.requestLocationPermissionIfNeeded()
+    init() {
+        locationManager.$latitude
+            .combineLatest(locationManager.$longitude)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] (lat: Double, lon: Double) in
+                guard lat != 0.0, lon != 0.0 else { return }
+                guard let self = self, !self.didSetCenter else { return }
+                self.centerCoordinate = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+                self.didSetCenter = true
+            }
+            .store(in: &cancellables)
     }
 }
